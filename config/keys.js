795880ff8f9bd34b24dceba7c789082e77447a71
vicecarloans <huyst1997@icloud.com>
2018-12-10 23:51:05 -0500

    Fixing to deploy to heroku

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-    mongoURI: "mongodb://huydam:secret123@ds159273.mlab.com:59273/video-store",
-    cookiePrivateKey: "secret",
-    JWT_KEY: "secret",
-    CLOUDINARY_NAME: "stackoverflow",
-    CLOUDINARY_API: "421223359525716",
-    CLOUDINARY_SECRET: "zQU8JDzMLb4nMST-epFTPSQX8no"
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+}else{
+    module.exports = require('./dev');
 }
---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    cookiePrivateKey: process.env.COOKIE_PRIVATE_KEY,
+    JWT_KEY: process.env.JWT_KEY,
+    CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+    CLOUDINARY_API: process.env.CLOUDINARY_API,
+    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET
+}
